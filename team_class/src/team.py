9a75e4475de5d1242a8aedfc4cc03bcb5e22bdbd
John Basile <ashton.basile@gmail.com>
2023-07-10 14:13:04 +0100

    I completed the extension and all tests have passed

--- team_class/src/team.py
+++ team_class/src/team.py
@@ -3,6 +3,7 @@ class Team:
 		self.name = name
 		self.players = players
 		self.coach = coach
+		self.points = 0
 
 	def add_player(self, name):
 		self.players.append(name)
@@ -10,3 +11,6 @@ class Team:
 	def has_player(self, name):
 		return name in self.players
 
+	def play_game(self, outcome):
+		if outcome:
+			self.points += 3
